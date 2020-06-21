9d55cc3d855e6afb84ea034470bd35c51b19866f
Raed Ismail <raid.ismail.s@gmail.com>
2020-06-20 22:30:31 -0700

    First commit

---
+++ src/components/design/Display.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import './Display.css';
+
+const Display = () => {
+
+    return (
+ <div className="card card-content"> 
+ <div className="imgTshirt text-center">
+     <img 
+     className="img responsive"
+     src="https://res.cloudinary.com/dkkgmzpqd/image/upload/v1545217305/T-shirt%20Images/black.png"
+     alt="img-Tshirt"
+     />
+     </div>
+     <div className="memeText text-center">
+         <div className="upperText">
+             <p>Upper Text</p>
+         </div>
+         <img 
+            src="http://via.placeholder.com/400x300"
+            alt="meme-text"
+         />
+         <div className="lowerText">
+             <p>Lower Text</p>
+         </div>
+     </div>
+ </div>  
+   )
+}
+
+export default Display;
