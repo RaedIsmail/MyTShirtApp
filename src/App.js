beb5868e3eab983ac5941e2fc27610156434b8b7
Raed Ismail <raid.ismail.s@gmail.com>
2020-07-07 22:37:52 -0700

    AddingNavbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-    <Dashboard />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Dashboard />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
